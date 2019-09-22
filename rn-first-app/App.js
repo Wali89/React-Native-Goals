c944911e00cae22875ae15a295a6ab49ad2edb9f
Wali <wali.rahman89@yahoo.com>
2019-09-22 18:06:29 -0400

    added touch to delete

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
@@ -12,6 +12,12 @@ export default function App() {
     setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: goalTitle}]);
   };
 
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  };
+
   return (
 
     <View style={styles.screen}>
@@ -21,8 +27,14 @@ export default function App() {
       <FlatList
       keyExtractor={(item, index) => item.id}  
       data={courseGoals} 
-      renderItem={itemData => <GoalItem title={itemData.item.value}/>} 
-      />
+      renderItem={itemData => (
+      <GoalItem 
+        id={itemData.item.id} 
+        onDelete={removeGoalHandler} 
+        title={itemData.item.value}
+        />
+        )} 
+      /> 
 
     </View>
   );
